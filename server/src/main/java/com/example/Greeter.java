37cdfd671a0d76baf3e9dc8283cfc9920001d350
Jannick <jl-p@hotmail.com>
2019-11-11 14:56:43 +0100

    greet method in Greeter

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   /**
    * {@inheritDoc}
    */
-  public String greet(final String someone) {
+  final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
